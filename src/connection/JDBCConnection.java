e0307c6b466d4696b755a393ae8006be21848692
TruongTra <nhattruong25041999@gmail.com>
2020-07-18 14:47:24 +0700

    Tạo class connection
    dùng class này để kết nối database, đang code cứng, sửa sau

---
+++ src/connection/JDBCConnection.java
@@ -0,0 +1,24 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCConnection {
+	public static Connection getJDBCConnection() {
+		String url = "jdbc:mysql://localhost:3306/db_control";
+		String user = "root";
+		String password = "";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
