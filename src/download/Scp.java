096429ee7f3a7d6b00ccea6d8dbbb0b0ab45b079
Slo <sloversion2@gmail.com>
2020-07-26 17:05:08 +0700

    add function to read from config, write to log and check if process already running

--- src/download/Scp.java
+++ src/download/Scp.java
@@ -1,33 +1,66 @@
 package download;
 
-import java.io.*;
+import connection.JDBCConnection;
+
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class Scp {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        String cmd = "src/download/ScpScript.zsh";
+    public static void main(String[] args) throws IOException, InterruptedException, SQLException {
+        Connection connection = JDBCConnection.getConnection("db_control");
+        Statement processStatement = connection.createStatement();
+        ResultSet resultSetProcess = processStatement.executeQuery("SELECT status FROM process WHERE process_name = 'download'");
+        resultSetProcess.next();
+        if (resultSetProcess.getString("status").equals("pending")) {
+            return;
+        }
+        resultSetProcess.close();
+        processStatement.close();
+        PreparedStatement processPreparedStatement = connection.prepareStatement("UPDATE `db_control`.`process` SET status=? WHERE (`process_name` = 'download');");
+        processPreparedStatement.setString(1, "pending");
+        processPreparedStatement.execute();
+        String configID = args[0];
+        Statement configStatement = connection.createStatement();
+        ResultSet resultSet = configStatement.executeQuery("SELECT Regrex FROM config WHERE id=" + configID);
+        resultSet.next();
+        String regrex = resultSet.getString("Regrex");
+        resultSet.close();
+        configStatement.close();
+        String[] cmd = {"src/download/ScpScript.zsh", regrex};
         ProcessBuilder processBuilder = new ProcessBuilder(cmd);
         Process process = processBuilder.start();
         process.waitFor();
         File tempFolder = new File("temp");
         File[] listFileTemp = tempFolder.listFiles();
-        BufferedReader logReader = new BufferedReader(new FileReader("src/download/download.log"));
-        String log;
         ArrayList<String> listFileDownloaded = new ArrayList<>();
-        PrintWriter logWriter = new PrintWriter(new FileWriter("src/download/download.log", true));
-        while ((log = logReader.readLine()) != null) {
-            String[] string = log.split(" ");
-            listFileDownloaded.add(string[2]);
+        Statement logStatement = connection.createStatement();
+        ResultSet resultSetLog = logStatement.executeQuery("SELECT File_Name FROM log WHERE Id_File=" + configID);
+        while (resultSetLog.next()) {
+            listFileDownloaded.add(resultSetLog.getString("File_Name"));
         }
+        resultSetLog.close();
+        logStatement.close();
+        PreparedStatement preparedStatementLog = connection.prepareStatement("INSERT INTO `db_control`.`log` (Id_File, Status, File_Name, Source_File, Type_File, Time) VALUES (?,?,?,?,?,?)");
         for (File file : listFileTemp) {
             if (!listFileDownloaded.contains(file.getName())) {
                 Files.copy(file.toPath(), new File("data/" + file.getName()).toPath());
-                logWriter.println(new Timestamp(System.currentTimeMillis()) + " " + file.getName());
+                preparedStatementLog.setString(1, configID);
+                preparedStatementLog.setString(2, "ER");
+                preparedStatementLog.setString(3, file.getName());
+                preparedStatementLog.setString(4, "data/" + file.getName());
+                String[] temp = file.getName().split("\\.");
+                preparedStatementLog.setString(5, temp[1]);
+                preparedStatementLog.setString(6, new Timestamp(System.currentTimeMillis()) + "");
+                preparedStatementLog.execute();
             }
         }
-        logWriter.close();
-
+        preparedStatementLog.close();
+        processPreparedStatement.setString(1, "finished");
+        processPreparedStatement.execute();
+        processPreparedStatement.close();
+        connection.close();
     }
 }
--- src/download/ScpScript.zsh
+++ src/download/ScpScript.zsh
@@ -2,5 +2,4 @@
 
 password="123456"
 username="guest_access"
-file="drive.ecepvn.org:/volume1/ECEP/song.nguyen/DW_2020/data/sinhvien_{chieu,sang}_nhom*.xlsx"
-sshpass -p "$password" scp -P 2227 $username@$file temp/
+sshpass -p "$password" scp -P 2227 $username@drive.ecepvn.org:/volume1/ECEP/song.nguyen/DW_2020/data/$1 temp/
