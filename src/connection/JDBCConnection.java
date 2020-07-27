c3d7e8f306cb65102f2f5f09908a41460088bc3d
TruongTra <nhattruong25041999@gmail.com>
2020-07-27 10:33:18 +0700

    Update class ReadFile
    cập nhật trạng thái process và trạng thái log

--- src/connection/JDBCConnection.java
+++ src/connection/JDBCConnection.java
@@ -7,9 +7,11 @@ public class JDBCConnection {
 		try {
 			String url = "jdbc:mysql://localhost:3306/";
 			String user = "root";
-			String password = "$Ngay22031999";
+//			String password = "$Ngay22031999";
+			String password = "";
 			url = url + database;
-			Class.forName("com.mysql.cj.jdbc.Driver");
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection(url, user, password);
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
--- src/loadFile/ReadFile.java
+++ src/loadFile/ReadFile.java
@@ -10,16 +10,29 @@ import java.sql.Statement;
 
 public class ReadFile {
     public static void readFile(int idFile) throws IOException, ClassNotFoundException, SQLException {
+    	//check process
+    	Connection connectionProcess = JDBCConnection.getConnection("db_control");
+        Statement statementProcess = connectionProcess.createStatement();
+        ResultSet resultSetProcess = statementProcess.executeQuery("SELECT * FROM process;");
+        if (resultSetProcess.next()) {
+            String status = resultSetProcess.getString(2);
+            if(!status.equals("finished")) {
+            	return;
+            }
+        }
+        String sql2 = "UPDATE process SET process.status = 'pending' WHERE process.process_name ='loadfile';";
+        statementProcess.executeUpdate(sql2);
+        //loadfile
         Connection connection = JDBCConnection.getConnection("db_control");
         Statement statementControl = connection.createStatement();
         //lấy file trong log theo idFile và có trạng thái ER
         ResultSet resultSetLog = statementControl.executeQuery("SELECT * FROM log WHERE log.Status= 'ER' AND log.Id_File = " + idFile + " LIMIT 1;");
         if (resultSetLog.next()) {
             String sourceFile = resultSetLog.getString(5);
-            String fileStatus = resultSetLog.getString(3);
+            String nameFile = resultSetLog.getString(4);
             String typeFile = resultSetLog.getString(6);
             //lấy định loại file trong control vd sinhvien
-            ResultSet resultSet = statementControl.executeQuery("SELECT* FROM config WHERE config.Id_File= " + idFile);
+            ResultSet resultSet = statementControl.executeQuery("SELECT* FROM config WHERE config.id= " + idFile);
             if (resultSet.next()) {
                 String destination = resultSet.getString(3);
                 //tất cả field của loại file
@@ -30,14 +43,24 @@ public class ReadFile {
                 }else if(typeFile.equals("txt")){
                     ReadTXT.readValuesTXT(destination,fields, sourceFile, delimiter);
                 }
-
             }
+            String sqlUpdateStatusLog ="UPDATE log SET log.Status = 'loaded' WHERE log.File_Name='"+nameFile+"';";
+            statementControl.execute(sqlUpdateStatusLog);
         }
         statementControl.close();
         connection.close();
+        //update process status
+        sql2 = "UPDATE process SET process.status = 'finished' WHERE process.process_name ='loadfile';";
+        statementProcess.executeUpdate(sql2);
+        statementProcess.close();
+        connectionProcess.close();
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
-        readFile(1);
+       for(String s :args)
+       {
+    	   int n = Integer.parseInt(s);
+    	   readFile(n);
+       }
     }
 }
