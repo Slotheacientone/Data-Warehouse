35d9c7748fe33409207a12f2816ada1ea5770ff7
TruongTra <nhattruong25041999@gmail.com>
2020-08-02 11:39:01 +0700

    Update class readexcel
    Sửa lỗi đọc những dòng không đủ thông tin. Chỉ chấp nhận load những dòng đã điền đủ thông tin

--- src/loadFile/ReadExcel.java
+++ src/loadFile/ReadExcel.java
@@ -2,11 +2,14 @@ package loadFile;
 
 import connection.JDBCConnection;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -17,111 +20,112 @@ import java.util.Iterator;
 
 public class ReadExcel {
 
-    public static void readExcel(String destination, String fields, String sourceFile) throws IOException, ClassNotFoundException, SQLException {
-        //tạo câu query
-        String sql = "INSERT INTO students (";
-        String[] arFiels = fields.split("\\,");
-        for (int i = 0; i < arFiels.length; i++) {
-            if (i == 0) {
-                sql += arFiels[i];
-            } else {
-                sql += "," + arFiels[i];
-            }
-        }
-        sql += ") VALUES (";
-        for (int i = 0; i < arFiels.length - 1; i++) {
-            if (i == 0) {
-                sql += "?";
-            } else {
-                sql += ",?";
-            }
-        }
-        sql += ",'Null')";
-        Connection connection = JDBCConnection.getConnection(destination);
-        PreparedStatement statement2 = connection.prepareStatement(sql);
-        //lấy dữ liệu file.xlsx
-        FileInputStream fileInputStream = new FileInputStream(sourceFile);
-        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
-        XSSFSheet sheet = workbook.getSheetAt(0);
-        Iterator<Row> rowIterator = sheet.iterator();
-        rowIterator.next();
+	public static void readExcel(String destination, String fields, String sourceFile)
+			{
+		// tạo câu query1 cho những câu không có ghi chú và query 2 cho câu có ghi chú
+		String sql = "INSERT INTO students (";
+		String[] arFiels = fields.split("\\,");
+		for (int i = 0; i < arFiels.length; i++) {
+			if (i == 0) {
+				sql += arFiels[i];
+			} else {
+				sql += "," + arFiels[i];
+			}
+		}
+		sql += ") VALUES (";
+		for (int i = 0; i < arFiels.length - 1; i++) {
+			if (i == 0) {
+				sql += "?";
+			} else {
+				sql += ",?";
+			}
+		}
+		String sql2 = sql;
+		sql2 += ",?)";
+		sql += ",'Null')";
 
-        while (rowIterator.hasNext()) {
-            // lấy phần tử hiện tại
-            Row nextRow = rowIterator.next();
-            Iterator<Cell> cellIterator = nextRow.cellIterator();
+		Connection connection = JDBCConnection.getConnection(destination);
+		PreparedStatement statement2 = null;
 
-            while (cellIterator.hasNext()) {
-                Cell nextCell = cellIterator.next();
+		// lấy dữ liệu file.xlsx
+		FileInputStream fileInputStream;
+		try {
+			fileInputStream = new FileInputStream(sourceFile);
+			// đưa fis về dạng file excel
+			XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+			 
+			// đọc sheet thứ nhất
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			FormulaEvaluator formula = workbook.getCreationHelper().createFormulaEvaluator();
+			Iterator<Row> rowIterator = sheet.iterator();
+			rowIterator.next();
+			while (rowIterator.hasNext()) {
+				// lấy phần tử hiện tại
+				Row nextRow = rowIterator.next();
+				int last = nextRow.getLastCellNum();
+				if (last == arFiels.length + 1) {
+					
+						statement2 = connection.prepareStatement(sql2);
+					 
+				} else if (last == arFiels.length) {
+					statement2 = connection.prepareStatement(sql);
+				} else {
+					continue;
+				}
+				Iterator<Cell> cellIterator = nextRow.cellIterator();
+				int count = 0;
+				while (cellIterator.hasNext()) {
+					Cell nextCell = cellIterator.next();
+					CellType cellType = nextCell.getCellType();
+					int columnIndex = nextCell.getColumnIndex();
+					if (columnIndex != 0) {
+						count++;
+						switch (cellType) {
+						case NUMERIC:
+							int mssvInt = (int) nextCell.getNumericCellValue();
+							String mssv = "" + mssvInt;
+							statement2.setString(columnIndex, mssv);
+							break;
+						case STRING:
+							String lastName = nextCell.getStringCellValue();
+							statement2.setString(columnIndex, lastName);
+							break;
+						default:
+							statement2.setString(columnIndex, "Null");
+							break;
+						}
+					}
 
-                int columnIndex = nextCell.getColumnIndex();
+				}
+				if (count + 1 == last) {
+					statement2.execute();
+				}
+			}
+			workbook.close();
+			fileInputStream.close();
+			statement2.close();
+			connection.close();
+			System.out.println("Done!");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Err!");
+			e.printStackTrace();
+			
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Err!");
+			e.printStackTrace();
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Err!");
+			e.printStackTrace();
+		}
+		
 
-                switch (columnIndex) {
-                    case 1:
-                        int mssvInt = (int) nextCell.getNumericCellValue();
-                        String mssv = "" + mssvInt;
-                        statement2.setString(1, mssv);
-                        break;
-                    case 2:
-                        String lastName = nextCell.getStringCellValue();
-                        statement2.setString(2, lastName);
-                        break;
-                    case 3:
-                        String firstName = nextCell.getStringCellValue();
-                        statement2.setString(3, firstName);
-                        break;
-                    case 4:
-                        Date dateOfBirth = nextCell.getDateCellValue();
-                        statement2.setString(4, getString(dateOfBirth));
-                        break;
-                    case 5:
-                        String classID = nextCell.getStringCellValue();
-                        statement2.setString(5, classID);
-                        break;
-                    case 6:
-                        String className = nextCell.getStringCellValue();
-                        statement2.setString(6, className);
-                        break;
+	}
 
-                    case 7:
-                        int sdtInt = (int) nextCell.getNumericCellValue();
-                        String sdt = "" + sdtInt;
-                        statement2.setString(7, sdt);
-                        break;
-                    case 8:
-                        String email = nextCell.getStringCellValue();
-                        statement2.setString(8, email);
-                        break;
-                    case 9:
-                        String queQuan = nextCell.getStringCellValue();
-                        statement2.setString(9, queQuan);
-                        break;
-                    case 10:
-                        System.out.println("abc");
-                        String note = "Null";
-//					if(nextCell.getCellType()!=null)
-//					{
-//						note += nextCell.getStringCellValue();
-//					}else {
-//						note += "Null";
-//					}
-                        statement2.setString(10, note);
-                        break;
-                }
-
-            }
-            statement2.execute();
-
-        }
-        workbook.close();
-        fileInputStream.close();
-        statement2.close();
-        connection.close();
-        System.out.println("Done!");
-
-    }
-
-    public static String getString(Date d) {
-        return new SimpleDateFormat("yyyy-MM-dd").format(d);
-    }
+	public static String getString(Date d) {
+		return new SimpleDateFormat("yyyy-MM-dd").format(d);
+	}
 }
